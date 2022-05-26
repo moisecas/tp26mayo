e0957f23389fc649f0d402b0f6b6655e2b9e8f06
Your Name <moisesjuniorcas20@gmail.com>
2022-05-26 11:06:50 -0500

    backend

---
+++ data/products.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+       _id: '1',
+        name: 'skin gow',
+        image: '/images/gow.jpg',
+        description: 'Para ps5',
+        brand:'techcenter',
+        price: 10,
+        countInStock:3,
+        rating:4.5,
+        numReviews:4,
+
+    },
+    {
+        _id: '2',
+        name: 'skin gow1',
+        image: '/images/gow1.jpg',
+        description: 'Para ps5',
+        brand:'techcenter',
+        price: 10,
+        countInStock:3,
+        rating:4.5,
+        numReviews:4,
+    },
+    {
+        _id: '3',
+        name: 'skin gow2',
+        image: '/images/gow2.jpg',
+        description: 'Para ps5',
+        brand:'techcenter',
+        price: 10,
+        countInStock:3,
+        rating:4.5,
+        numReviews:4,
+    },
+];
+module.exports = products;

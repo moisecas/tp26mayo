62355643e69e6346ba7620711104841e98974ac1
Your Name <moisesjuniorcas20@gmail.com>
2022-07-19 17:02:25 -0500

    se agrega dependencia colors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import conectDB from './config/db.js';
+import connectDB from './config/db.js';
+import colors from 'colors'; 
 
 import products from './data/products.js';
 
@@ -27,4 +28,6 @@ const PORT = process.env.PORT || 5000;
 
 
 //servidor corriendo 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)); 
+app.listen(PORT, console.log
+    (`Server running in ${process.env.NODE_ENV} on port ${PORT}`
+    .green.underline.bold)); 

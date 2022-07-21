430a12f4cbd6cf3e6d3b85dc70395c0dee2a2890
Your Name <moisesjuniorcas20@gmail.com>
2022-07-21 11:24:43 -0500

    models comunicación con base de datos

---
+++ seeder.js
@@ -0,0 +1,66 @@
+import mongoose  from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js"; //importar la funcion connectDB de config/db para conectar a la base de datos y crearla si no existe
+
+
+dotenv.config();
+
+connectDB(); //conectar a la base de datos
+
+const importData = async () => {
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        const createdUsers = await User.insertMany(users);
+
+        const adminUser = createdUsers[0];
+
+        const sampleProducts = products.map(product => {
+            return{...product, user: adminUser}
+        })
+
+        await Product.insertMany(sampleProducts);
+
+        console.log('data imported'.green.inverse); 
+        process.exit();
+
+
+        
+    } catch (error) {
+        console.error(`Error: ${error}`.red);
+        process.exit(1);
+    }
+}
+
+const destroyData = async () => {
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        
+
+        console.log('destroyed'.red.inverse); 
+        process.exit();
+
+
+        
+    } catch (error) {
+        console.error(`Error: ${error}`.red);
+        process.exit(1);
+    }
+}
+
+if(process.argv[2] === '-d'){
+    destroyData();
+}else{
+    importData();   
+}

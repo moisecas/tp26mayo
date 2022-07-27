c43231112267931460d6346e8da5b8bd2e35c6e7
Your Name <moisesjuniorcas20@gmail.com>
2022-07-27 14:43:41 -0500

    agregando produc detail

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -4,6 +4,9 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants';
 
 
@@ -17,6 +20,19 @@ export const listProducts = () => async (dispatch) => {
 
     } catch (error) { //si hay un error en el servidor se ejecuta esta funcion y se envia el error
         dispatch({ type: PRODUCT_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message, });
+    }
+}
+
+export const listProductsDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+        const { data } = await axios.get(`/api/products/${id}`);
+
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+
+    } catch (error) { //si hay un error en el servidor se ejecuta esta funcion y se envia el error
+        dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message, });
     }
 }
 
